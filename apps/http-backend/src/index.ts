1549cb0d033486260a637408d0079465e98a6f13
Ankan Biswas <ankanbiswas365@gmail.com>
2025-12-28 20:50:13 +0530

    deleted some packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,19 +1,37 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
     //db call
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!!data.success) {
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
     res.json({
         userId: 123
     })
 
 })
 app.post("/signin", (req, res) => {
+
+    const data = SigninSchema.safeParse(req.body);
+    if (!!data.success) {
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
 
     const userId = 1;
     const token =  jwt.sign({
@@ -27,6 +45,16 @@ app.post("/signin", (req, res) => {
 })
 app.post("/room", middleware,  (req, res) => {
     // db call
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!!data.success) {
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
+
 
     res.json({
         roomId: 123
